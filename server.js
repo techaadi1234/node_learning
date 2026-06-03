f919eaf7637aa22bd28ff99b4c92d3e22318d2bb
techaadi1234 <aditya.240101173@iiitbh.ac.in>
2026-06-03 16:16:06 +0530

    learnt how to perform basic routing without express using switch cases

--- server.js
+++ server.js
@@ -4,7 +4,26 @@ const server = http.createServer((req,res)=>{
     console.log(req.url ,req.method);
 
     res.setHeader('Content-Type','text/html');
-    fs.readFile('./views/index.html',(err,data)=>{
+    let path='./views/';
+    switch(req.url){
+        case '/':
+        path+='index.html';
+        res.statusCode=200;
+        break;
+        case '/about':
+            path+='about.html';
+            res.statusCode=200;
+            break;
+            case '/about-me':
+                res.statusCode=301;
+                res.setHeader('Location','/about');
+                res.end();
+                default:
+                    path+='404.html';
+                    res.statusCode=404;
+                    break;
+    }
+    fs.readFile(path,(err,data)=>{
         if(err){
             console.log(err);
         }else{
