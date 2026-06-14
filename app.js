54a64d8f67d0c60c42fb2b55db5b3c8a4c55b0a1
techaadi1234 <aditya.240101173@iiitbh.ac.in>
2026-06-14 17:03:33 +0530

    initiated mongo db and mongoose

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express=require('express');
+const mongoose =require('mongoose');
+
+
+
 const app=express();
-app.listen(3000);
+
+
+
+const dbURI='mongodb+srv://aadi:1173@nodelearn.5lz3yxn.mongodb.net/?appName=nodelearn';
+mongoose.connect(dbURI)
+.then((result)=>app.listen(3000))
+.catch((err)=>console.log('error'))
+app.set('view engine','ejs');
+
+
 
 app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html',{root:__dirname});
+    res.render('index',{title:'HOME'});
 });
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname});
+  res.status(404).render('404',{title:'404'});
 
 });
